75527d0563ed0ea97c3c0fa2f1e2c332ae3e40a5
pedroheck <pedroscheck@hotmail.com>
2021-09-27 18:19:54 -0300

    Time Guessing game
    A simple console game for trying to correctly guess how many seconds has passed.

---
+++ Introduction to Game Dev/Assets/TimeGame.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeGame : MonoBehaviour {
+
+    float roundStartDelayTime = 3;
+    
+    float roundStartTime;
+    int waitTime;
+    bool roundStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Press the spacebar once you think the allotted time is up.");
+
+        Invoke("SetNewRandomTime", roundStartDelayTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && roundStarted) // If player presses the spacebar
+        {
+            InputRecieved();   
+        }
+    }
+
+    void InputRecieved()
+    {
+        roundStarted = false;
+        float playerWaitTime = Time.time - roundStartTime;
+        float error = Mathf.Abs(waitTime - playerWaitTime);
+
+        print("You waited for " + playerWaitTime + " seconds. That's " + error + " seconds off. " + GenerateMessage(error);
+
+        Invoke("SetNewRandomTime", roundStartDelayTime);
+    }
+
+    string GenerateMessage(float error)
+    {
+        string message = "";
+        if (error < .15f)
+        {
+            message = "Outstanding!";
+        }
+        else if (error < .75f)
+        {
+            message = "Exceeds Expactations.";
+        }
+        else if (error < 1.25f)
+        {
+            message = "Acceptable.";
+        }
+        else
+        {
+            message = "Dreadful.";
+        }
+
+        return message;
+    }
+
+    void SetNewRandomTime()
+    {
+        waitTime = Random.Range(5, 21);
+        roundStartTime = Time.time;
+
+        roundStarted = true;
+
+        print(waitTime + " seconds.");
+    }
+}
