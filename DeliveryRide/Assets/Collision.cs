f8d0b2abe0e909fc6fc789a43cc07331e697d29f
pedroheck <pedroscheck@hotmail.com>
2022-02-22 15:51:51 -0300

    Adding assets for the game

--- DeliveryRide/Assets/Collision.cs
+++ DeliveryRide/Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("What was that?");
+    }
 }
