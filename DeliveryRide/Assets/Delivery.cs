79524fffcdacd30c5023c66fee91316200613a2e
pedroheck <pedroscheck@hotmail.com>
2022-02-22 18:55:56 -0300

    Beginning delivery system by adding Customer and Package tags

--- DeliveryRide/Assets/Delivery.cs
+++ DeliveryRide/Assets/Delivery.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("What was that?");
+        if (other.tag == "Package"){
+            Debug.Log("Package picked up!");
+        }
+        if (other.tag == "Customer"){
+            Debug.Log("Package Delivered!");
+        }
     }
 }
