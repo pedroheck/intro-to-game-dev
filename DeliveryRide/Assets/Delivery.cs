4069ed970b18cf6b8022c1054331a291580375fe
pedroheck <pedroscheck@hotmail.com>
2022-02-23 10:28:42 -0300

    Destroying package and changing player's color when picking up package

--- DeliveryRide/Assets/Delivery.cs
+++ DeliveryRide/Assets/Delivery.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 0.1f;
     bool hasPackage; // By default it starts as false
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Package"){
+        if (other.tag == "Package" && !hasPackage){ // Only allows to pick up package if the player doesn't have a package already
             Debug.Log("Package picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
         if (other.tag == "Customer" && hasPackage){
             Debug.Log("Package Delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
