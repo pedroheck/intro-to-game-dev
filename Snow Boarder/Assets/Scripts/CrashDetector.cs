01b36d171676309d225bf54d4dc5cb29beaf28f3
pedroheck <pedroscheck@hotmail.com>
2022-03-21 11:05:36 -0300

    Reloading scene when finishing or crashing

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 0.5f;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Ground"){
-            Debug.Log("Ouch!");
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    private void ReloadScene(){
+        SceneManager.LoadScene(0); // Loads scene of index 0 (in our case, Scene1)
     }
 }
--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1f;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
-            Debug.Log("You finished first! You are the only one playing but still");
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    private void ReloadScene(){
+        SceneManager.LoadScene(0); // Loads scene of index 0 (in our case, Scene1)
     }
 }
